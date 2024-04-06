7fdbbbb913433b356fb519dbb367cd5998377249
traindoggo <traindoggo@nyasan.com>
2024-04-06 23:20:33 +0900

    compe: contest abc348 abcd

---
+++ AtCoder/abc/348/B/solve.cc
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define rrep(i,a,n) for (int i=a; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<pair<double, double>> vp;
+  rep(_, n) {
+    double x, y;
+    cin >> x >> y;
+    vp.push_back({x, y});
+  }
+
+  for (int i = 0; i < n; ++i) {
+    double dist{-1};
+    int idx{};
+    for (int j = 0; j < n; ++j) {
+      if (i == j) continue;
+      double x = vp[i].first - vp[j].first;
+      double y = vp[i].second - vp[j].second;
+
+      double cur = x * x + y * y;
+      if (chmax(dist, cur)) {
+        idx = j + 1;
+      }
+    }
+
+    cout << idx << el;
+  }
+}
