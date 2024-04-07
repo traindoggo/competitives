b1387d0e436b3b0f77055fbbc30ec3e7829268ee
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-04-07 13:07:15 +0900

    atcoder: apg4b

---
+++ AtCoder/apg4b/cd/solve.cc
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define rrep(i,a,n) for (int i=a; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+// 参照渡しを用いて、呼び出し側の変数の値を変更する
+void saiten(vector<vector<int>>& A, int& correct_count, int& wrong_count) {
+  // 呼び出し側のAの各マスを正しい値に修正する
+  for (int i = 1; i <= 9; ++i) {
+    for (int j = 1; j <= 9; ++j) {
+      if (i * j == A[i - 1][j - 1]) {
+        correct_count++;
+      } else {
+        A[i - 1][j - 1] = i * j;
+      }
+    }
+  }
+
+  // Aのうち、正しい値の書かれたマスの個数を correct_count に入れる
+  // Aのうち、誤った値の書かれたマスの個数を wrong_count に入れる
+
+  wrong_count = 9 * 9 - correct_count;
+  // ここにプログラムを追記
+}
+
+// -------------------
+// ここから先は変更しない
+// -------------------
+int main() {
+  // A君の回答を受け取る
+  vector<vector<int>> A(9, vector<int>(9));
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cin >> A.at(i).at(j);
+    }
+  }
+
+  int correct_count = 0;  // ここに正しい値のマスの個数を入れる
+  int wrong_count = 0;    // ここに誤った値のマスの個数を入れる
+
+  // A, correct_count, wrong_countを参照渡し
+  saiten(A, correct_count, wrong_count);
+
+  // 正しく修正した表を出力
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cout << A.at(i).at(j);
+      if (j < 8)
+        cout << " ";
+      else
+        cout << endl;
+    }
+  }
+  // 正しいマスの個数を出力
+  cout << correct_count << endl;
+  // 誤っているマスの個数を出力
+  cout << wrong_count << endl;
+}
