55f68e865c5c1ce7e473b98d0d89eba34cde61ae
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-04-12 20:50:47 +0900

    atcoder: tessoku a03, a05

---
+++ AtCoder/tessoku-book/A05/solve.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define rrep(i,a,n) for (int i=a; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  int cnt{};
+  for (int red = 1; red <= n; ++red) {
+    for (int blue = 1; blue <= n; ++blue) {
+      int white = k - red - blue;
+      if (1 <= white && white <= n) {
+        cnt++;
+      }
+    }
+  }
+  cout << cnt << el;
+}
