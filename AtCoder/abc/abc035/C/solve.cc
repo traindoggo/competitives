9aef09826d6d99dec8164b7ff40bd4ae94740941
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-04-13 14:17:00 +0900

    atcoder abc035: c

---
+++ AtCoder/abc/abc035/C/solve.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0; i<(int)n; ++i)
+#define rrep(i,a,n) for (int i=a; i<(int)n; ++i)
+#define out(msg) cout << (msg) << '\n'
+#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+// const
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+// alias
+using ullint = unsigned long long int;
+using llint  = long long int;
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+// clang-format on
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+
+  vector<int> board(n + 1, 0);
+
+  rep(_, q) {
+    int l, r;
+    cin >> l >> r;
+    l--;
+    board[l]++;
+    board[r]--;
+  }
+
+  rep(i, n) board[i + 1] += board[i];
+  dump(board);
+
+  rep(i, n) {
+    if (board[i] % 2 == 0)
+      cout << 0;
+    else
+      cout << 1;
+  }
+  cout << el;
+}
