c67d0f79ed4b94db6290a066f28b2e7e17b8569d
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-04-27 11:31:25 +0900

    atcoder: abc248 d

---
+++ AtCoder/abc/abc248/D/solve.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using llint  = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> ai(n);
+  rep(i, n) cin >> ai[i];
+
+  map<int, vector<int>> idx;
+  rep(i, n) {
+    idx[ai[i]].push_back(i);
+  }
+
+  int q;
+  cin >> q;
+  rep(_, q) {
+    int l, r, x;
+    cin >> l >> r >> x;
+
+    auto left = lower_bound(all(idx[x]), l - 1);
+    auto right = lower_bound(all(idx[x]), r);
+    cout << right - left << el;
+  }
+}
