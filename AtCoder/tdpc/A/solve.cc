a3a6e3da276056d09d724a57c44f62b4dc825258
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-06 02:11:17 +0900

    tdpc: a

---
+++ AtCoder/tdpc/A/solve.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> pi(n);
+  rep(i, n) cin >> pi[i];
+
+  const int W = 100 * 100;
+  vector dp(n + 1, vector<bool>(W + 1, false));
+  dp[0][0] = true;
+
+  rep(i, n) {
+    rep(j, W + 1) {
+      if (!dp[i][j]) continue;
+      dp[i + 1][j] = true;
+      if (j + pi[i] <= W) {
+        dp[i + 1][j + pi[i]] = true;
+      }
+    }
+  }
+
+  int ans{};
+  rep(i, W + 1) {
+    if (dp[n][i]) ans++;
+  }
+  cout << ans << el;
+}
