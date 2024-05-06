a958f03fb61d114c92c48cf72ba7e345c86c63ff
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-06 11:25:50 +0900

    alds1 1_11_a

---
+++ aoj/alds1/1_11_a/solve.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector matrix(n, vector<int>(n, 0));
+
+  rep(_, n) {
+    int u, k;
+    cin >> u >> k;
+
+    rep(j, k) {
+      int v;
+      cin >> v;
+      matrix[u - 1][v - 1] = 1;
+    }
+  }
+
+  rep(i, n) {
+    rep(j, n) {
+      cout << matrix[i][j] << (j == n - 1 ? '\n' : ' ');
+    }
+  }
+}
