6046ebab008b2eab88d0bddc3ac22cfe10582065
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-12 17:24:57 +0900

    abc212 c

--- AtCoder/abc/abc212/C/solve.cc
+++ AtCoder/abc/abc212/C/solve.cc
@@ -10,24 +10,17 @@ using namespace std;
 // clang-format off
 struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define rep(i,n) for (int i=0; i<(int)n; ++i)
-#define rrep(i,a,n) for (int i=a; i<(int)n; ++i)
-#define out(msg) cout << (msg) << '\n'
-#define die(msg) do{ cout << (msg) << endl,exit(0); }while(0)
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
 #define el '\n'
 
 #define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
 
-// const
 #define INFi  1   << 30
 #define INFll 1LL << 60
 #define MOD17 10'0000'0007
 #define MOD98  9'9824'4353
-
-// alias
-using ullint = unsigned long long int;
-using llint  = long long int;
 
 template <typename T> inline bool chmax(T& a, const T& b) {
   return ((a < b) ? (a = b, true) : false);
@@ -35,6 +28,8 @@ template <typename T> inline bool chmax(T& a, const T& b) {
 template <typename T> inline bool chmin(T& a, const T& b) {
   return ((a > b) ? (a = b, true) : false);
 }
+
+using ll = long long int;
 // clang-format on
 
 int main() {
@@ -44,18 +39,18 @@ int main() {
   vector<int> ai(n), bi(m);
   rep(i, n) cin >> ai[i];
   rep(i, m) cin >> bi[i];
-
   sort(all(ai));
   sort(all(bi));
 
   int ans{INFi};
-  int a{}, b{};
-  while (a < n && b < m) {
-    chmin(ans, abs(ai[a] - bi[b]));
-    if (ai[a] > bi[b]) {
-      b++;
+  int l{}, r{};
+  while (l < n && r < m) {
+    chmin(ans, abs(ai[l] - bi[r]));
+
+    if (ai[l] < bi[r]) {
+      l++;
     } else {
-      a++;
+      r++;
     }
   }
   cout << ans << el;
