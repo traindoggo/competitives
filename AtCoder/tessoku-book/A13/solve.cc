98082ca7efec83c31add43d0bd4a86c185f87940
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-19 20:47:14 +0900

    tessoku: a13 lower bound, syakutori

---
+++ AtCoder/tessoku-book/A13/solve.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> ai(n);
+  rep(i, n) cin >> ai[i];
+  sort(all(ai));
+
+  ll ans{};
+  rep(i, n) {
+    ll key = ai[i] + k;
+    ll left{}, right{n};
+
+    while (right - left > 1) {
+      ll mid = left + (right - left) / 2;
+      if (ai[mid] <= key) {
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+
+    /* dump(i, left, right); */
+    ans += right - i - 1;
+  }
+  cout << ans << el;
+}
