839c896eacd1b36e4ddfb450be3128624e2d5509
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-22 23:40:17 +0900

    abc308 d

--- AtCoder/abc/abc308/D/solve.cc
+++ AtCoder/abc/abc308/D/solve.cc
@@ -13,57 +13,59 @@ struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 #define rep(i,n) for (int i=0;i<(int)n;++i)
 #define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
 #define el '\n'
+#define YesNo(f) cout<<(f?"Yes":"No")<<el
 
-#define all(k)  k.begin(),  k.end()
-#define rall(k) k.rbegin(), k.rend()
-
-// const
-#define INFi  1   << 30
-#define INFll 1LL << 60
-#define MOD17 10'0000'0007
-#define MOD98  9'9824'4353
-
-// alias
-using ullint = unsigned long long int;
-using llint  = long long int;
+using ll = long long int;
 // clang-format on
 
-int dh[]{0, -1, 1, 0};
-int dw[]{-1, 0, 0, 1};
+int dy[]{0, 1, -1, 0};
+int dx[]{1, 0, 0, -1};
+
+using P = pair<int, int>;
 
 int main() {
   int h, w;
   cin >> h >> w;
 
+  string snuke{"snuke"};
+
   vector<string> vs(h);
   rep(i, h) cin >> vs[i];
+  if (vs[0][0] != 's') die("No");
 
-  string snuke{"snuke"};
-  vector visited(h, vector<int>(w, false));
+  auto nxt = [&](int y, int x) {
+    char ch = vs[y][x];
+    int idx = snuke.find(ch);
+    return snuke[(idx + 1) % 5];
+  };
 
-  auto dfs = [&](auto f, int y, int x, int step) -> void {
-    visited[y][x] = true;
-    step %= 5;
+  vector<vector<bool>> seen(h, vector<bool>(w, false));
 
-    rep(i, 4) {
-      int nh = y + dh[i];
-      int nw = x + dw[i];
+  auto dfs = [&](int sy, int sx) {
+    seen[sy][sx] = true;
 
-      if (!(0 <= nh and nh < h and 0 <= nw and nw < w)) continue;
+    // use stack - 普通にWAりそう
+    stack<P> st;
+    st.push({sy, sx});
 
-      // current -> next
-      if (snuke[step] != vs[y][x]) continue;
-      if (snuke[(step + 1) % 5] != vs[nh][nw]) continue;
-      if (visited[nh][nw]) continue;
+    while (!st.empty()) {
+      auto [y, x] = st.top();
+      st.pop();
 
-      f(f, nh, nw, step + 1);
+      rep(i, 4) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (!(0 <= ny && ny < h && 0 <= nx && nx < w)) continue;
+        if (vs[ny][nx] != nxt(y, x)) continue;
+        if (seen[ny][nx]) continue;
+        seen[ny][nx] = true;
+        st.push({ny, nx});
+      }
     }
   };
 
-  dfs(dfs, 0, 0, 0);
+  dfs(0, 0);
 
-  if (visited[h - 1][w - 1])
-    cout << "Yes" << el;
-  else
-    cout << "No" << el;
+  YesNo(seen[h - 1][w - 1]);
 }
