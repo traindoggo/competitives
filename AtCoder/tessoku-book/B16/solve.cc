c810125403c1e9fcb83448b0007108ccb2cf3616
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-25 14:43:49 +0900

    tessoku: b16

---
+++ AtCoder/tessoku-book/B16/solve.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> hi(n);
+  rep(i, n) cin >> hi[i];
+
+  vector<int> dp(n + 1, INFi);
+  dp[0] = 0;
+
+  rep(i, n) {
+    if (i < n - 1) {
+      chmin(dp[i + 1], dp[i] + abs(hi[i] - hi[i + 1]));
+    }
+
+    if (i < n - 2) {
+      chmin(dp[i + 2], dp[i] + abs(hi[i] - hi[i + 2]));
+    }
+  }
+
+  cout << dp[n - 1] << el;
+}
