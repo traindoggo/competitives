69c7a566a69d473e4f7031961a09f6a7d26727ee
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-25 19:15:23 +0900

    tessoku: a26

---
+++ AtCoder/tessoku-book/A26/solve.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) cout<<(msg)<<el;exit(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int q;
+  cin >> q;
+
+  vector<int> xi(q);
+  rep(i, q) cin >> xi[i];
+
+  auto is_prime = [&](const int& n) -> bool {
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) return false;
+    }
+    return true;
+  };
+
+  for (const int& x : xi) {
+    if (is_prime(x)) {
+      cout << "Yes" << el;
+    } else {
+      cout << "No" << el;
+    }
+  }
+}
