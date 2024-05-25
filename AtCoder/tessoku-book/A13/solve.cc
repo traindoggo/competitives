9a70becbd9fc466bcc7beeb2ec31c07bb8d43510
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-25 17:44:39 +0900

    tessoku: a13

--- AtCoder/tessoku-book/A13/solve.cc
+++ AtCoder/tessoku-book/A13/solve.cc
@@ -16,7 +16,6 @@ struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
 #define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
-
 #define INFi  1   << 30
 #define INFll 1LL << 60
 #define MOD17 10'0000'0007
@@ -33,27 +32,33 @@ using ll = long long int;
 // clang-format on
 
 int main() {
-  ll n, k;
+  int n, k;
   cin >> n >> k;
 
-  vector<ll> ai(n);
+  vector<int> ai(n);
   rep(i, n) cin >> ai[i];
 
-  ll ans{};
-  rep(i, n) {
-    ll cur = ai[i] + k;
+  auto lowerbound = [&](int key) {
+    int left{}, right{(int)ai.size()};
 
-    ll left{0}, right{n};
     while (right - left > 1) {
-      int mid = left + (right - left) / 2;
-      if (ai[mid] <= cur) {
+      int mid = (right + left) / 2;
+
+      if (ai[mid] <= key) {
         left = mid;
       } else {
         right = mid;
       }
     }
 
-    ans += right - 1 - i;
+    return right;
+  };
+
+  ll ans{};
+
+  rep(i, n) {
+    int idx = lowerbound(ai[i] + k);
+    ans += idx - i - 1;
   }
 
   cout << ans << el;
