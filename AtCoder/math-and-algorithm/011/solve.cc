c9657009a9c028a7a4aee17d07b3a356b981eacc
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-26 21:46:24 +0900

    math-algo: 011 prime

---
+++ AtCoder/math-and-algorithm/011/solve.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) cout<<(msg)<<el;exit(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+bool is_prime(int n) {
+  if (n == 2 || n == 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
+
+  for (int i = 3; i * i <= n; i += 2) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> ans;
+  for (int i = 2; i <= n; ++i) {
+    if (is_prime(i)) {
+      ans.push_back(i);
+    }
+  }
+
+  for (const int& a : ans) {
+    cout << a << el;
+  }
+}
