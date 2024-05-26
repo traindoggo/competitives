4413039e8e5ba8ed5ed591f04fcb516dc51fe6bb
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-27 01:22:12 +0900

    math-algo: 019

---
+++ AtCoder/math-and-algorithm/019/solve.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) cout<<(msg)<<el;exit(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<ll> ai(3);
+  rep(i, n) {
+    int a;
+    cin >> a;
+    ai[a - 1]++;
+  }
+
+  ll ans{};
+  for (const ll& a : ai) {
+    ans += a * (a - 1) / 2;
+  }
+  cout << ans << el;
+}
