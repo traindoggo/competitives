e7c8dec6395f755d616f266410941d4a5ff83059
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-27 20:12:30 +0900

    typical90 004

--- AtCoder/typical90/004/solve.cc
+++ AtCoder/typical90/004/solve.cc
@@ -10,13 +10,12 @@ using namespace std;
 // clang-format off
 struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define rep(i,n) for (int i=0;i<(int)n;++i)
-#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
 #define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) cout<<(msg)<<el;exit(0);
 
 #define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
-
 #define INFi  1   << 30
 #define INFll 1LL << 60
 #define MOD17 10'0000'0007
@@ -36,25 +35,29 @@ int main() {
   int h, w;
   cin >> h >> w;
 
-  vector<vector<int>> grid(h, vector<int>(w, 0));
-  rep(i, h) rep(j, w) cin >> grid[i][j];
+  vector<vector<int>> cells(h, vector<int>(w, 0));
+  rep(i, h) rep(j, w) cin >> cells[i][j];
 
-  vector<int> row(h, 0), col(w, 0);
+  vector<int> row(h), col(w);
   rep(i, h) {
     int sum{};
-    rep(j, w) sum += grid[i][j];
+    rep(j, w) sum += cells[i][j];
     row[i] = sum;
   }
 
   rep(j, w) {
     int sum{};
-    rep(i, h) sum += grid[i][j];
+    rep(i, h) sum += cells[i][j];
     col[j] = sum;
   }
 
   rep(i, h) {
     rep(j, w) {
-      cout << row[i] + col[j] - grid[i][j] << (j != w - 1 ? ' ' : '\n');
+      int sum = row[i] + col[j] - cells[i][j];
+
+      cout << sum;
+      if (j < w - 1) cout << ' ';
     }
+    cout << el;
   }
 }
