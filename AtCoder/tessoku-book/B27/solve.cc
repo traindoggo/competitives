c020da17aa18628fa3dc4e2e2638f1674c5a4686
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-28 23:54:51 +0900

    tessoku: b27

---
+++ AtCoder/tessoku-book/B27/solve.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<el;exit(0);}while(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+namespace me {
+ll gcd(ll a, ll b) {
+  if (b == 0) return a;
+  if (a < b) swap(a, b);
+  return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+  return a / gcd(a, b) * b;
+}
+}  // namespace me
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+
+  cout << lcm(a, b) << el;
+}
