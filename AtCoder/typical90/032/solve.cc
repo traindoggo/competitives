62af436ea8d75cb211da9db2f875d20170cbcc32
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-29 19:45:19 +0900

    typical90 032

---
+++ AtCoder/typical90/032/solve.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<el;exit(0);}while(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+  vector runners(n, vector<ll>(n, 0));
+  rep(i, n) rep(j, n) cin >> runners.at(i).at(j);
+
+  vector bad(n, vector<bool>(n, false));
+  int m;
+  cin >> m;
+  rep(_, m) {
+    int x, y;
+    cin >> x >> y;
+    x--, y--;
+    bad[x][y] = true;
+    bad[y][x] = true;
+  }
+
+  vector<int> nums;
+  rep(i, n) nums.push_back(i);
+
+  ll ans{INFll};
+
+  do {
+    bool is_skip{false};
+
+    rep(i, n - 1) {
+      if (bad[nums[i]][nums[i + 1]] || bad[nums[i + 1]][nums[i]]) {
+        is_skip = true;
+      }
+    }
+
+    if (!is_skip) {
+      ll sum{};
+      rep(i, n) sum += runners[nums[i]][i];
+      chmin(ans, sum);
+    }
+  } while (next_permutation(all(nums)));
+
+  if (ans == INFll)
+    cout << -1 << el;
+  else
+    cout << ans << el;
+}
