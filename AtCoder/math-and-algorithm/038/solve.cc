6b42eca771c1f167a555092fba99f995b9560449
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-01 19:45:56 +0900

    math-algo: 038

---
+++ AtCoder/math-and-algorithm/038/solve.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<el;exit(0);}while(0);
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+
+  vector<ll> ai(n);
+  rep(i, n) cin >> ai[i];
+
+  vector<ll> acc(n + 1);
+  rep(i, n) acc[i + 1] = acc[i] + ai[i];
+
+  rep(_, q) {
+    int l, r;
+    cin >> l >> r;
+    cout << acc[r] - acc[l - 1] << el;
+  }
+}
