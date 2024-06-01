94c130e440585c491839be34653283bf7b813cb1
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-01 20:04:40 +0900

    tessoku: b11

--- AtCoder/tessoku-book/B11/solve.cc
+++ AtCoder/tessoku-book/B11/solve.cc
@@ -10,13 +10,12 @@ using namespace std;
 // clang-format off
 struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define rep(i,n) for (int i=0;i<(int)n;++i)
-#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
 #define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<el;exit(0);}while(0);
 
 #define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
-
 #define INFi  1   << 30
 #define INFll 1LL << 60
 #define MOD17 10'0000'0007
@@ -35,14 +34,17 @@ using ll = long long int;
 namespace me {
 int lower_bound(const vector<int>& ai, int key) {
   int left{-1}, right{(int)ai.size()};
+
   while (right - left > 1) {
     int mid = left + (right - left) / 2;
-    if (key <= ai[mid]) {
+    if (ai[mid] >= key) {
       right = mid;
     } else {
       left = mid;
     }
   }
+
+  dump(left, right);
   return right;
 }
 }  // namespace me
