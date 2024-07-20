c376aac822791b8f2c1f24ddac676215caeb1101
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-21 01:23:32 +0900

    31611

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -7,22 +7,9 @@ use proconio::{input, marker::Chars, source::line::LineSource};
 fn main() {
     //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-    let mut teams: Vec<(usize, usize)> = Vec::new();
-    teams.push((12, 1600));
-    teams.push((11, 894));
-    teams.push((11, 1327));
-    teams.push((10, 1311));
-    teams.push((9, 1004));
-    teams.push((9, 1178));
-    teams.push((9, 1357));
-    teams.push((8, 837));
-    teams.push((7, 1055));
-    teams.push((6, 556));
-    teams.push((6, 773));
-
     input! {
-        idx: usize,
+        x: i32,
     }
-    let team = teams[idx - 1];
-    println!("{} {}", team.0, team.1);
+
+    println!("{}", if x % 7 == 2 { 1 } else { 0 });
 }
