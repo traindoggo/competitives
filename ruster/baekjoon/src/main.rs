588c75e219e8d019dc9129cd6b2e14beb09b4f77
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-20 22:45:18 +0900

    fix rust version

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -1,11 +1,17 @@
+use std::io::{stdout, BufWriter, Write};
+
 #[allow(unused)]
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a: f64,
-        b: f64,
+        n: i32,
     }
 
-    println!("{:.10}", a / b);
+    let out = stdout();
+    let mut out = BufWriter::new(out.lock());
+
+    for i in 1..=n {
+        out.write(format!("{}\n", i).as_bytes()).unwrap();
+    }
 }
