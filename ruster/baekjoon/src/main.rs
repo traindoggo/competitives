c21c8ba21f8b0f3fc9435768e62c7606ff9544ec
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-21 01:15:26 +0900

    5337

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -7,15 +7,8 @@ use proconio::{input, marker::Chars, source::line::LineSource};
 fn main() {
     //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-
-    input! {
-        patient:String,
-        doctor: String,
-    }
-
-    if patient.len() < doctor.len() {
-        println!("no");
-    } else {
-        println!("go");
-    }
+    let ans = r".  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.";
+    println!("{}", ans);
 }
