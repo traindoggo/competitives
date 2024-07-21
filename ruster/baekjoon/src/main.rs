9de0027bde0d0f9ef2ac969fa628b79cfba98668
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 00:07:34 +0900

    7891

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -7,16 +7,15 @@ use proconio::{input, marker::Chars, source::line::LineSource};
 fn main() {
     //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-
     input! {
-        nums: [i32; 5],
+        n: usize,
     }
 
-    let mut ans: i32 = 0;
-
-    for num in nums {
-        ans += num;
+    for _ in 0..n {
+        input! {
+            a: i64,
+            b: i64,
+        }
+        println!("{}", a + b);
     }
-
-    println!("{}", ans);
 }
