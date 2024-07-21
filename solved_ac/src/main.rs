4fd8a9a9133a0b85834982835c5a73cb7d268f80
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 02:24:44 +0900

    8958

--- solved_ac/src/main.rs
+++ solved_ac/src/main.rs
@@ -1,19 +1,24 @@
-use std::collections::HashSet;
-
 #[allow(unused)]
 use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        nums: [i32; 10],
-    }
-    eprintln!("{:?}", nums);
-
-    let mut st: HashSet<i32> = HashSet::new();
-
-    for num in nums {
-        st.insert(num % 42);
+        n: usize,
+        marks: [Chars;n],
     }
 
-    println!("{}", st.len());
+    for line in marks {
+        let mut total: i32 = 0;
+        let mut cnt: i32 = 0;
+
+        for ch in line {
+            if ch == 'O' {
+                cnt += 1;
+            } else {
+                cnt = 0;
+            }
+            total += cnt;
+        }
+        println!("{}", total);
+    }
 }
