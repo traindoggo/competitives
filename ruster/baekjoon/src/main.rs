df5c1ddbab3c473abbc52594fb4e62ef9bd94c78
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-21 23:50:44 +0900

    5522

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -5,17 +5,18 @@ use std::io::{self, BufReader};
 use proconio::{input, marker::Chars, source::line::LineSource};
 
 fn main() {
-    // let mut stdin = LineSource::new(BufReader::new(io::stdin()));
-    // macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-    let c3po = r"     /~\
-    ( oo|
-    _\=/_
-   /  _  \
-  //|/.\|\\
- ||  \ /  ||
-============
-|          |
-|          |
-|          |";
-    println!("{}", c3po);
+    //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
+    //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
+
+    input! {
+        nums: [i32; 5],
+    }
+
+    let mut ans: i32 = 0;
+
+    for num in nums {
+        ans += num;
+    }
+
+    println!("{}", ans);
 }
