d0eed949214ddd6567e495be7eb58172bac0066d
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 00:28:06 +0900

    10171

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -7,16 +7,9 @@ use proconio::{input, marker::Chars, source::line::LineSource};
 fn main() {
     //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-    input! {
-        score: i32,
-    }
-
-    let ans = match score {
-        90..=100 => 'A',
-        80..=89 => 'B',
-        70..=79 => 'C',
-        60..=69 => 'D',
-        _ => 'F',
-    };
-    println!("{}", ans)
+    let cat = r"\    /\
+ )  ( ')
+(  /  )
+ \(__)|";
+    println!("{}", cat);
 }
