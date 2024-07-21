51d0403d9bd1e78a6347a053f30f94a57eb26178
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-21 23:41:11 +0900

    5339

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -7,10 +7,15 @@ use proconio::{input, marker::Chars, source::line::LineSource};
 fn main() {
     // let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     // macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
-    input! {
-        x: i32,
-        y: i32,
-    }
-
-    println!("{}", x * 60 + y);
+    let c3po = r"     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |";
+    println!("{}", c3po);
 }
