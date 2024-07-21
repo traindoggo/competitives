f738a1dbcf6a8044a49cf6fb589d87155ae1c3c9
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 02:04:09 +0900

    31403

--- solved_ac/src/main.rs
+++ solved_ac/src/main.rs
@@ -3,9 +3,30 @@ use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        a: i64,
-        b: i64,
+        a: String,
+        b: String,
+        c: String,
     }
 
-    println!("{}", a * b);
+    add1(&a, &b, &c);
+    add2(&a, &b, &c);
+}
+
+fn add1(a: &String, b: &String, c: &String) {
+    let mut sum: i32 = 0;
+    sum += a.parse::<i32>().unwrap();
+    sum += b.parse::<i32>().unwrap();
+    sum -= c.parse::<i32>().unwrap();
+    println!("{}", sum);
+}
+
+fn add2(a: &String, b: &String, c: &String) {
+    let mut k = String::new();
+    k.push_str(a);
+    k.push_str(b);
+
+    let mut sum: i32 = 0;
+    sum += k.parse::<i32>().unwrap();
+    sum -= c.parse::<i32>().unwrap();
+    println!("{}", sum);
 }
