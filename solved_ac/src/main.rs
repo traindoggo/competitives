f8fc43dfa2ef35275bb5ddb94143d21f51fe20a6
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 01:27:10 +0900

    25083

--- solved_ac/src/main.rs
+++ solved_ac/src/main.rs
@@ -2,8 +2,16 @@
 use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {
-        ch : char,
+    let ans: Vec<&str> = vec![
+        "         ,r'\"7",
+        "r`-_   ,'  ,/",
+        " \\. \". L_r'",
+        "   `~\\/",
+        "      |",
+        "      |",
+    ];
+
+    for line in ans {
+        println!("{}", line);
     }
-    println!("{}", ch as u8);
 }
