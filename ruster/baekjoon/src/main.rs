e5a5694f990815557d4193cfe90c83bc69647950
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-22 00:23:37 +0900

    10808

--- ruster/baekjoon/src/main.rs
+++ ruster/baekjoon/src/main.rs
@@ -8,10 +8,21 @@ fn main() {
     //let mut stdin = LineSource::new(BufReader::new(io::stdin()));
     //macro_rules! input(($($tt:tt)*) => (proconio::input!(from &mut stdin, $($tt)*)));
     input! {
-        n: i32,
-        s: i32,
+        s: Chars
     }
 
-    let diff = s - n;
-    println!("{}", n + 2 * diff);
+    let mut counter = vec![0; 26];
+
+    for ch in s {
+        let idx = (ch as u8 - 'a' as u8) as usize;
+        counter[idx] += 1;
+    }
+    println!(
+        "{}",
+        counter
+            .iter()
+            .map(|num| num.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
